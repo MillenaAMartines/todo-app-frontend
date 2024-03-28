f15099c1113ec8063ac76b96cc6d342f80c3dfad
Millena Araújo Martines <millenamartinez@Henriques-MacBook-Air.local>
2024-03-28 15:48:45 -0300

    implementaçã da extruturaçõa

---
+++ src/about/about.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+import PageHeader from "../templete/pageheader";
+
+export default props => (
+    <div>
+      <PageHeader name='Sobre' small='Nós'></PageHeader>
+      <h2>Nossa História</h2>
+      <p>Inicio no mundo da programação em 2022..... </p>
+      <h2>Missão e visão</h2>
+      <p>Incentivar a todos .....</p>
+      <h2>Imprensa</h2>
+      <p>aberto </p>
+
+    </div>
+) 

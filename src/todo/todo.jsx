3a605aa3541cbde9699879ace6ccdb03e21d0c9f
Millena Araújo Martines <millenamartinez@Henriques-MacBook-Air.local>
2024-04-01 15:35:17 -0300

    Estruturação do Formulario

--- src/todo/todo.jsx
+++ src/todo/todo.jsx
@@ -5,12 +5,20 @@ import TodoForm from './todoForm'
 import TodoList from './todoList'
 
 export default class Todo extends Component {
+    constructor(props) {
+        super(props)
+        this.handleAdd = this.handleAdd.bind(this)
+    }
+
+    handleAdd() {
+        console.log(this)
+    }
     render() {
         return (
             <div>
                 <PageHeader name='Tarefas' small='Cadastro' />
-                <TodoForm/>
-                <TodoList/>
+                <TodoForm handleAdd={this.handleAdd} />
+                <TodoList />
             </div>
         )
     }
--- src/todo/todoForm.jsx
+++ src/todo/todoForm.jsx
@@ -1,7 +1,17 @@
 import React from 'react'
+import Grid from '../templete/grid'
+import IconButton from '../templete/iconButton'
 
-export default props => ( 
-    <div>
-        <h1>Form</h1>
+export default props => (
+    <div role='for' className='todoForm'>
+        <Grid cols='12 9 10'>
+            <input id='description' className='form-control'
+                placeholder='Adcione uma tarefa'></input>
+        </Grid>
+        <Grid cols='12 3 2'>
+            <IconButton style='primary' icon='plus'
+             onClick={props.handleAdd}></IconButton>
+        </Grid>
+
     </div>
 )

99ec4fd7ab73dd8e02ef8a78689c22c98c632a2b
Millena Araújo Martines <millenamartinez@Henriques-MacBook-Air.local>
2024-04-05 12:11:16 -0300

    add funcionalidade de pesquisa

--- src/todo/todoList.jsx
+++ src/todo/todoList.jsx
@@ -3,19 +3,26 @@ import IconButton from '../templete/iconButton'
 
 export default props => {
 
-    const renderRows =() => { 
+    const renderRows = () => {
         const list = props.list || []
-        return list.map(todo => ( 
+        return list.map(todo => (
             <tr key={todo._id}>
-                <td>{todo.description}</td>
+                <td className={todo.done ? 'markedAsDone' : ''}>{todo.description}</td>
                 <td>
-                    <IconButton style='danger' icon='trash-o'
-                    onClick={() => props.handleRemove(todo)}></IconButton>
+                    <IconButton
+                        style='success' icon='check' hide={todo.done}
+                        onClick={() => props.handleMarkAsDone(todo)}></IconButton>
+                    <IconButton
+                        style='warning' icon='undo' hide={!todo.done}
+                        onClick={() => props.handleMarkAsPending(todo)}></IconButton>
+                    <IconButton
+                        style='danger' icon='trash-o' hide={!todo.done}
+                        onClick={() => props.handleRemove(todo)}></IconButton>
                 </td>
             </tr>
         ))
     }
-    return(
+    return (
         <table className='table'>
             <thead>
                 <tr>
@@ -29,4 +36,3 @@ export default props => {
         </table>
     )
 }
-   

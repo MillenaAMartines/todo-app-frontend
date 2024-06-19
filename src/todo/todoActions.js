1eb97f6054bdbbbb291e336750a4a74fa37671e9
Millena Araújo Martines <millenamartinez@Henriques-MacBook-Air.local>
2024-06-19 17:12:23 -0300

    acrescentando Middleware

--- src/todo/todoActions.js
+++ src/todo/todoActions.js
@@ -1,4 +1,18 @@
-export const changeDescription = (event) => ({
+import axios from "axios"
+
+const URL = 'http://localhost:3003/api/todos'
+
+export const changeDescription = event => ({
     type: 'DESCRIPTION_CHANGED',
     payload: event.target.value
 })
+
+// chamada assincrona
+export const search = () => {
+    const request = axios.get(`${URL}?sort=-createdAt`)
+    return {
+        type: 'TODO_SEARCHED',
+        payload: request
+    }
+}
+
